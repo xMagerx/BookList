c56f3e8938383bc121af0c5bc208321ac2fd1c8c
xMagerx <32936738+xMagerx@users.noreply.github.com>
2017-11-16 10:34:53 +0200

    something
    Signed-off-by: xMagerx <32936738+xMagerx@users.noreply.github.com>

---
+++ workspace(16 00)/Hello World/src/ua/lviv/hello/HelloWorldLesson.java
@@ -0,0 +1,21 @@
+package ua.lviv.hello;
+
+public class HelloWorldLesson {
+	
+	public static void main(String[] args) {
+		//This will print hello world
+		System.out.println("Hello World!");
+//		int var = 65;
+//		
+//		float withComma = 65.5f;
+//		
+//		float withCommaSecond = (float) 65.5;
+//		
+//		long newVar = 85483566767548666l;
+//		
+//		var = (int) newVar;
+//		
+//		System.out.println(var);
+	}
+	
+}
